ae2b055839d93b60bd6f117893d0394d796c1d51
tomit4 <mosssap@gmail.com>
2023-01-28 02:49:53 -0800

    :construction: Set up seeding of db through node

--- research/db/sqlite.js
+++ research/db/sqlite.js
@@ -1,27 +1,83 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('states.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+const data = require("../states_alabama.json")
+
+const db = new sqlite3.Database('../states.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
     if (err) return console.error(err.message)
     console.log('Connected to the in-memory SQlite database.')
 });
 
-// db.serialize (() => {
-    // db.run(`CREATE TABLE IF NOT EXISTS peoples(
-        // id TEXT NOT NULL UNIQUE,
-        // name TEXT NOT NULL,
-        // message TEXT NOT NULL)`,
-    // )
+        // Assign foreign keys and create separate tables for each:
+        // senators TEXT NOT NULL,
+        // house_delegation TEXT NOT NULL,
+db.serialize (() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS states(
+        name TEXT NOT NULL,
+        date_admitted TEXT NOT NULL,
+        capital TEXT NOT NULL,
+        largest_city TEXT NOT NULL,
+        govenor TEXT NOT NULL,
+        area_total TEXT NOT NULL,
+        area_land TEXT NOT NULL,
+        area_water TEXT NOT NULL,
+        elevation TEXT NOT NULL,
+        population_total TEXT NOT NULL,
+        population_density TEXT NOT NULL,
+        time_zone TEXT NOT NULL,
+        latitude TEXT NOT NULL,
+        longitude TEXT NOT NULL,
+        url TEXT NOT NULL,
+        flag_url TEXT NOT NULL,
+        insignia_url TEXT NOT NULL)`,
+    )
 
-    // db.run(
-        // `INSERT OR IGNORE INTO peoples VALUES
-        // (1, 'Brad Pitt', 'this site is dope'),
-        // (2, 'Jennifer Lopez', 'this site is ok')`,
-    // )
-// })
-
+    data.forEach((item) => {
+        const sqlStmt = db.prepare(`
+            INSERT OR IGNORE INTO states (
+                name,
+                date_admitted,
+                capital,
+                largest_city,
+                govenor,
+                area_total,
+                area_land,
+                area_water,
+                elevation,
+                population_total,
+                population_density,
+                time_zone,
+                latitude,
+                longitude,
+                url,
+                flag_url,
+                insignia_url)
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+        `)
+        sqlStmt.run(
+            `${item.name}`,
+            `${item.date_admitted}`,
+            `${item.capital}`,
+            `${item.largest_city}`,
+            `${item.govenor}`,
+            `${item.area_total}`,
+            `${item.area_land}`,
+            `${item.area_water}`,
+            `${item.elevation}`,
+            `${item.population_total}`,
+            `${item.population_density}`,
+            `${item.time_zone}`,
+            `${item.latitude}`,
+            `${item.longitude}`,
+            `${item.url}`,
+            `${item.flag_url}`,
+            `${item.insignia_url}`
+        )
+    })
+})
 
 module.exports = db
 
 // db.close((err) => {
     // if (err) return console.error(err.message)
-    // console.log('Closing the database connecttion...')
+    // console.log('Closing the database connection...')
 // });
