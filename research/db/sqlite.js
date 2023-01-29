58187a5725731f9bbbbd01ed6cbfa242e3d7cd37
tomit4 <mosssap@gmail.com>
2023-01-29 01:22:58 -0800

    :pencil2: Modified routing for use with express

--- research/db/sqlite.js
+++ research/db/sqlite.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const data = require("../states_alabama.json")
 
-const db = new sqlite3.Database('../states.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+const db = new sqlite3.Database('./states.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
     if (err) return console.error(err.message)
     console.log('Connected to the in-memory SQlite database.')
 });
