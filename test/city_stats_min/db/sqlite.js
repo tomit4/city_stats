fae238c6e82d44c6479cad1d3f8179c20f6f68d9
tomit4 <mosssap@gmail.com>
2023-03-29 14:13:21 -0700

    :recycle: Refactored where insert statements are called

--- test/city_stats_min/db/sqlite.js
+++ test/city_stats_min/db/sqlite.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose()
 const sdb = require('./states.json')
 const cdb = require('./cities.json')
-const { createStmts } = require('../utils/sql.js')
+const { createStmts, insertStmts } = require('../utils/sql.js')
 
 const db = new sqlite3.Database(
     './db/metro_stats.db',
@@ -14,69 +14,8 @@ const db = new sqlite3.Database(
 
 db.serialize(() => {
     createStmts.forEach(stmt => db.run(stmt))
-    // TODO: parse these out into separate file somehow...
-    sdb.forEach((sd, i) => {
-        db.run(
-            `INSERT OR IGNORE INTO states VALUES
-            (${i + 1},
-            ${JSON.stringify(sd.state_name)},
-            ${JSON.stringify(sd.state_abbreviation)}, 
-            ${JSON.stringify(sd.date_admitted)}, 
-            ${JSON.stringify(sd.capital)}, 
-            ${JSON.stringify(sd.largest_city)}, 
-            ${JSON.stringify(sd.govenor)}, 
-            json('${JSON.stringify(sd.senators)}'),
-            json('${JSON.stringify(sd.house_delegation)}'),
-            json_object('total', '${sd.area.total}', 'land', '${
-                sd.area.land
-            }', 'water', '${sd.area.water}'),
-            ${JSON.stringify(sd.elevation)},
-            json_object('total', '${sd.population.total}', 'density', '${
-                sd.population.density
-            }', 'median_household_income', '${
-                sd.population.median_household_income
-            }'),
-            ${JSON.stringify(sd.time_zone)},
-            ${JSON.stringify(sd.latitude)},
-            ${JSON.stringify(sd.longitude)},
-            ${JSON.stringify(sd.url)},
-            ${JSON.stringify(sd.flag_url)},
-            ${JSON.stringify(sd.insignia_url)}
-            )`,
-        )
-    })
-
-    cdb.forEach((cd, i) => {
-        db.run(
-            `INSERT OR IGNORE INTO cities VALUES
-            (${i + 1},
-            ${JSON.stringify(cd.city_name)},
-            ${JSON.stringify(cd.state_name)},
-            ${JSON.stringify(cd.coordinates)},
-            json('${JSON.stringify(cd.counties)}'),
-            ${JSON.stringify(cd.settled_founded)},
-            ${JSON.stringify(cd.incorporated)},
-            json_object('type', '${cd.government.type}', 'mayor', '${
-                cd.government.mayor
-            }', 'city_council', json_array('${JSON.stringify(
-                cd.government.city_council,
-            )}')),
-            json_object('city', '${cd.area.city}', 'land', '${
-                cd.area.land
-            }', 'water', '${cd.area.water}'),
-            ${JSON.stringify(cd.elevation)},
-            json_object('city', '${cd.population.city}', 'density', '${
-                cd.population.density
-            }', 'metro', '${cd.population.metro}'),
-            ${JSON.stringify(cd.time_zone)},
-            json('${JSON.stringify(cd.zip_codes)}'),
-            json('${JSON.stringify(cd.area_codes)}'),
-            json('${JSON.stringify(cd.fips_code)}'),
-            json('${JSON.stringify(cd.gnis_feature_ids)}'),
-            ${JSON.stringify(cd.url)}
-            )`,
-        )
-    })
+    insertStmts.populateStates(sdb, db)
+    insertStmts.populateCities(cdb, db)
 })
 
 module.exports = db
--- test/city_stats_min/utils/sql.js
+++ test/city_stats_min/utils/sql.js
@@ -1,43 +1,110 @@
+// Helper function for less typing
+const _jfy = (data) => { return JSON.stringify(data) }
+
 const createStmts = [
     `CREATE TABLE IF NOT EXISTS states(
-        primary_key INTEGER PRIMARY KEY,
-        state_name TEXT,
-        state_abbreviation TEXT,
-        date_admitted TEXT,
-        capital TEXT,
-        largest_city TEXT,
-        govenor TEXT,
-        senators TEXT,
-        house_delegation TEXT,
-        area TEXT,
-        elevation TEXT,
-        population TEXT,
-        time_zone TEXT,
-        latitude TEXT,
-        longitutde TEXT,
-        url TEXT,
-        flag_url TEXT,
-        insignia_url TEXT
+    primary_key INTEGER PRIMARY KEY,
+    state_name TEXT,
+    state_abbreviation TEXT,
+    date_admitted TEXT,
+    capital TEXT,
+    largest_city TEXT,
+    govenor TEXT,
+    senators TEXT,
+    house_delegation TEXT,
+    area TEXT,
+    elevation TEXT,
+    population TEXT,
+    time_zone TEXT,
+    latitude TEXT,
+    longitutde TEXT,
+    url TEXT,
+    flag_url TEXT,
+    insignia_url TEXT
     )`,
     `CREATE TABLE IF NOT EXISTS cities(
-        primary_key INTEGER PRIMARY KEY,
-        city_name TEXT,
-        state_name TEXT,
-        coordinates TEXT,
-        counties TEXT,
-        settled_founded TEXT,
-        incorporated TEXT,
-        government TEXT,
-        area TEXT,
-        elevation TEXT,
-        population TEXT,
-        time_zone TEXT,
-        zip_codes TEXT,
-        area_codes TEXT,
-        fips_code TEXT,
-        gnis_feature_ids TEXT,
-        url TEXT
+    primary_key INTEGER PRIMARY KEY,
+    city_name TEXT,
+    state_name TEXT,
+    coordinates TEXT,
+    counties TEXT,
+    settled_founded TEXT,
+    incorporated TEXT,
+    government TEXT,
+    area TEXT,
+    elevation TEXT,
+    population TEXT,
+    time_zone TEXT,
+    zip_codes TEXT,
+    area_codes TEXT,
+    fips_code TEXT,
+    gnis_feature_ids TEXT,
+    url TEXT
     )`,
 ]
 
-module.exports = { createStmts }
+const insertStmts = {
+    populateStates: function(sdb, db) {
+        sdb.forEach((sd, i) => {
+            db.run(
+                `INSERT OR IGNORE INTO states VALUES (
+                ${i + 1},
+                ${_jfy(sd.state_name)},
+                ${_jfy(sd.state_abbreviation)}, 
+                ${_jfy(sd.date_admitted)}, 
+                ${_jfy(sd.capital)}, 
+                ${_jfy(sd.largest_city)}, 
+                ${_jfy(sd.govenor)}, 
+                json('${_jfy(sd.senators)}'),
+                json('${_jfy(sd.house_delegation)}'),
+                json_object('total', '${sd.area.total}', 'land', '${
+                sd.area.land}', 'water', '${sd.area.water}'),
+                ${_jfy(sd.elevation)},
+                json_object('total', '${sd.population.total}', 'density', '${
+                sd.population.density}', 'median_household_income', '${
+                sd.population.median_household_income}'),
+                ${_jfy(sd.time_zone)},
+                ${_jfy(sd.latitude)},
+                ${_jfy(sd.longitude)},
+                ${_jfy(sd.url)},
+                ${_jfy(sd.flag_url)},
+                ${_jfy(sd.insignia_url)})`
+            )
+        })
+    },
+    populateCities: function(cdb, db) {
+        cdb.forEach((cd, i) => {
+            db.run(
+                `INSERT OR IGNORE INTO cities VALUES(
+                ${i + 1},
+                ${_jfy(cd.city_name)},
+                ${_jfy(cd.state_name)},
+                ${_jfy(cd.coordinates)},
+                json('${_jfy(cd.counties)}'),
+                ${_jfy(cd.settled_founded)},
+                ${_jfy(cd.incorporated)},
+                json_object('type', '${cd.government.type}', 'mayor', '${
+                    cd.government.mayor
+                }', 'city_council', json_array('${_jfy(
+                    cd.government.city_council,
+                )}')),
+                json_object('city', '${cd.area.city}', 'land', '${
+                    cd.area.land
+                }', 'water', '${cd.area.water}'),
+                ${_jfy(cd.elevation)},
+                json_object('city', '${cd.population.city}', 'density', '${
+                    cd.population.density
+                }', 'metro', '${cd.population.metro}'),
+                ${_jfy(cd.time_zone)},
+                json('${_jfy(cd.zip_codes)}'),
+                json('${_jfy(cd.area_codes)}'),
+                json('${_jfy(cd.fips_code)}'),
+                json('${_jfy(cd.gnis_feature_ids)}'),
+                ${_jfy(cd.url)}
+                )`,
+            )
+        })
+    }
+}
+
+module.exports = {createStmts, insertStmts}
