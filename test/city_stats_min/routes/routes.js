0178c98227970f4c583a3977f5d9b4e6488c4351
tomit4 <mosssap@gmail.com>
2023-03-29 16:21:33 -0700

    :construction: Set up for returning all other query data

--- test/city_stats_min/routes/routes.js
+++ test/city_stats_min/routes/routes.js
@@ -3,17 +3,24 @@ const sdb = require('../db/states.json')
 const cdb = require('../db/cities.json')
 const { handleHeadersSentErr } = require('../utils/utils.js')
 const parser = require('../utils/parser.js')
+
 const routes = {
     stateNames: sdb.map(sd => sd.state_name),
+    stateKeys: Object.keys(sdb[0]),
     citiesNames: cdb.map(cd => cd.city_name),
+    cityKeys: Object.keys(cdb[0]),
     statesRouter: function (req, res, next) {
         const { query } = req.params
         switch (true) {
             case !query:
+                console.log(this.stateKeys)
                 returnAll('states', res, next)
                 break
             case this.stateNames.includes(query):
-                returnSingleInstanceOf('states', 'state', res, next, query)
+                returnSingleInstanceOf('states', res, next, query)
+                break
+            case this.stateKeys.includes(query):
+                returnAllSpecs('states', res, next, query)
                 break
             default:
                 res.send({ msg: `${query} not found...` })
@@ -26,7 +33,10 @@ const routes = {
                 returnAll('cities', res, next)
                 break
             case this.citiesNames.includes(query):
-                returnSingleInstanceOf('cities', 'city', res, next, query)
+                returnSingleInstanceOf('cities', res, next, query)
+                break
+            case this.cityKeys.includes(query):
+                returnAllSpecs('cities', res, next, query)
                 break
             default:
                 res.send({ msg: 'no query fields yet' })
@@ -34,15 +44,17 @@ const routes = {
     },
 }
 
-const returnAll = (entity, res, next) => {
-    db.all(`SELECT * FROM ${entity}`, [], (err, rows) => {
+// TODO: Export these functions out to another file, possibly put in object?
+const returnAll = (table, res, next) => {
+    db.all(`SELECT * FROM ${table}`, (err, rows) => {
         handleHeadersSentErr(res, err, next)
         parser.prettify(rows)
         return res.send(rows)
     })
 }
 
-const returnSingleInstanceOf = (table, instance, res, next, query) => {
+const returnSingleInstanceOf = (table, res, next, query) => {
+    const instance = table === 'states' ? 'state' : 'city'
     db.all(
         `SELECT * FROM ${table} WHERE ${instance}_name = ?`,
         [query],
@@ -52,6 +64,18 @@ const returnSingleInstanceOf = (table, instance, res, next, query) => {
             return res.send(rows)
         },
     )
+}
+
+const returnAllSpecs = (table, res, next, query) => {
+    const instance = table === 'states' ? 'state' : 'city'
+    db.all(
+        `SELECT ${instance}_name, ${query} FROM ${table}`, (err, rows) => {
+            handleHeadersSentErr(res, err, next)
+            parser.prettify(rows)
+            return res.send(rows)
+        }
+    )
+
 }
 
 module.exports = { routes }
