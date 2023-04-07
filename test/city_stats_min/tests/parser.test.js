54dc30d4aa96e929e529a28771d7a48603574aac
tomit4 <mosssap@gmail.com>
2023-04-06 21:54:16 -0700

    :white_check_mark: Set up first basic test, installed sinon

---
+++ test/city_stats_min/tests/parser.test.js
@@ -0,0 +1,33 @@
+// Simple test to see if prettify parses JSON properly
+const test = require('ava')
+// Mock data, json as string and parsed
+const mock = {
+    input: [
+        {
+            "senators": "[\"Tommy Tuberville\",\"Katie Britt\"]",
+            "house_delegation": "[\"Jerry Carl\",\"Barry Moore\"]",
+            "area": "{\"total\":\"52419 sq mi\",\"land\":\"50744 sq mi\",\"water\":\"1675 sq mi\"}",
+        },
+    ],
+    output: [
+        {
+            senators: ['Tommy Tuberville', 'Katie Britt'],
+            house_delegation: ['Jerry Carl', 'Barry Moore'],
+            area: {
+                total: '52419 sq mi',
+                land: '50744 sq mi',
+                water: '1675 sq mi',
+            },
+        },
+    ],
+}
+
+// Ava tests
+test('parser prettifies state object', async t => {
+    const { prettify } = require('../utils/parser.js')
+    t.deepEqual(prettify(mock.input), mock.output)
+})
+
+test('state object without prettify does not pass', async t => {
+    t.notDeepEqual(mock.input == mock.output)
+})
--- test/city_stats_min/utils/parser.js
+++ test/city_stats_min/utils/parser.js
@@ -1,16 +1,22 @@
 'use strict'
 // Prettifies Nested Values
 function prettify(rows) {
-    for (const key in rows)
-        if (typeof rows[key] === 'object')
-            for (const k in rows[key])
-                if (typeof rows[key][k] === 'string')
+    for (const key in rows) {
+        if (typeof rows[key] === 'object') {
+            for (const k in rows[key]) {
+                if (typeof rows[key][k] === 'string') {
                     if (rows[key][k].includes('{')) {
                         rows[key][k] = JSON.parse(rows[key][k])
                         Object.keys(rows[key][k]).forEach(() =>
                             prettify(rows[key][k]))
-                    } else if (rows[key][k].includes('['))
+                    } else if (rows[key][k].includes('[')) {
                         rows[key][k] = JSON.parse(rows[key][k])
+                    }
+                }
+            }
+        }
+    }
+    return rows
 }
 
 module.exports = { prettify }
