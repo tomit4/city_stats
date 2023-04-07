d21a4d46d420854dc60cfff25d993c32b4130396
tomit4 <mosssap@gmail.com>
2023-04-07 00:45:42 -0700

    :bug: Working on simple fix to bug

--- test/city_stats_min/db/sqlite.js
+++ test/city_stats_min/db/sqlite.js
@@ -14,6 +14,11 @@ const db = new sqlite3.Database(
     },
 )
 
+// NOTE: see sandbox major_cities.js.
+// You can solve this problem by using the array of objects generated
+// there. You may want to put this logic in another file under utils.js as it
+// specifically creates another json like structure to simply be inserted.
+
 db.serialize(() => {
     createStmts.forEach(stmt => db.run(stmt))
     insertStmts.populateStates(sdb, db)
@@ -22,7 +27,7 @@ db.serialize(() => {
     db.run(alterStmts.alter('states', 'major_cities'))
     // TODO: Due to synchronicity problems revealed by ava,
     // We'll have to refactor this by using javascript to parse through the
-    // states.jsona nd cities.json files first before sending it to a more
+    // states.json and cities.json files first before sending it to a more
     // simplified sql update statement
     sdb.forEach(sd =>
         alterStmts.update(db, 'city_name', 'cities', 'state_name',
---
+++ test/city_stats_min/sandbox/major_cities.js
@@ -0,0 +1,19 @@
+// To be removed along with sandbox folder later, quick fix for refactor
+
+const sdb = require('../db/states.json')
+const cdb = require('../db/cities.json')
+
+const majorCities = []
+sdb.forEach(sd => {
+    const singleStateWithCities = {}
+    singleStateWithCities.state_name = sd.state_name
+    singleStateWithCities.major_cities = []
+    cdb.forEach(cd => {
+        if (cd.state_name === sd.state_name) {
+            singleStateWithCities.major_cities.push(cd.city_name)
+        }
+    })
+    majorCities.push(singleStateWithCities)
+})
+
+console.log('majorCities >>', majorCities)
