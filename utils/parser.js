f725dceff672988df7e5dd9ac23deac363a686b3
tomit4 <mosssap@gmail.com>
2023-04-09 19:31:02 -0700

    :lipstick: Semantic change to prettify func

--- utils/parser.js
+++ utils/parser.js
@@ -1,25 +1,25 @@
 'use strict'
 // Recursively goes over any stringified JSON and parses it
 const prettify = (rows) => {
-    const deflate = (rows) => {
+    const parse = (rows) => {
         Object.keys(rows).forEach(row => {
             if (rows[row] == 'null') { rows[row] = null }
             if (typeof rows[row] === 'object' && rows[row]) {
-                deflate(rows[row])
+                parse(rows[row])
             } else if (typeof rows[row] === 'string') {
                 if (rows[row].includes('{') || rows[row].includes('[')) {
                     try {
                         rows[row] = JSON.parse(rows[row])
                         if (typeof rows[row] === 'object'
                             && rows[row])
-                            deflate(rows[row])
+                            parse(rows[row])
                     }
                     catch (err) {console.warn(`parsing error >>: ${err}`) }
                 }
             }
         })
     }
-    deflate(rows)
+    parse(rows)
     return rows
 }
 
