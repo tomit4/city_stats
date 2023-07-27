12529efc8b9058a01afc03d1d8651336ca56fefa
tomit4 <mosssap@gmail.com>
2023-07-27 00:57:32 -0700

    :sparkles: Finished cleaning up update_states.py

--- tests/scrapers/update_states.py
+++ tests/scrapers/update_states.py
@@ -32,17 +32,53 @@ def grab_data(new_reps_data, field):
     data = {}
     for key, value in new_reps_data[field].items():
         data[key] = value
+    # Edge case with district formatting
+    if field == 'District':
+        data = {key: value.split('\u00a0')[0] for key, value in data.items()}
     return data
 
 
-# TODO: break this out into smaller funcs
+def grab_new_info(dict, branch, new_reps):
+    new_rep_info = {}
+    for key, value in dict.items():
+        new_rep_info[key] = {'state_name': value, branch: new_reps[key]}
+    return new_rep_info
+
+
+def return_rep_list(new_info, new_rep, branch):
+    result = {}
+    for key, value in new_info.items():
+        state_name = value['state_name']
+        rep = value[new_rep]
+
+        if state_name not in result:
+            result[state_name] = {'state_name': state_name, branch: []}
+        result[state_name][branch].append(rep)
+    result_list = list(result.values())
+    return result_list
+
+
+def update_all(states_data, rep_result_list, sen_result_list):
+    for state in states_data:
+        state_name = state['state_name']
+        for result in rep_result_list:
+            if result['state_name'] == state_name:
+                state['house_delegation'] = result['house_delegation']
+        for sen_result in sen_result_list:
+            if sen_result['state_name'] == state_name:
+                state['senators'] = sen_result['senators']
+    # Save the updated states data back to states.json
+        with open('states.json', 'w') as json_file:
+            json.dump(states_data, json_file, indent=2)
+
+
 def update_reps():
     rm_if_exists('new_reps.json')
     rm_if_exists('new_sens.json')
     scrape_data(urls[0], 6, 'new_reps.json')
     scrape_data(urls[1], 5, 'new_sens.json')
 
-    # Load the my_data.json file containing the new representatives
+    # Load json files
     new_reps_data = grab_json('new_reps.json')
     new_sens_data = grab_json('new_sens.json')
     states_data = grab_json('states.json')
@@ -54,67 +90,13 @@ def update_reps():
     # Grab new senators' data
     new_sens = grab_data(new_sens_data, 'Senator')
     states = grab_data(new_sens_data, 'State')
+    new_sen_info = grab_new_info(states, 'senator', new_sens)
+    new_state_info = grab_new_info(districts, 'representative', new_reps)
 
-    new_sen_info = {}
-    for key, state in states.items():
-        new_sen_info[key] = {'state_name': state, 'senator': new_sens[key]}
-
-    sen_result_dict = {}
-    for key, value in new_sen_info.items():
-        state_name = value['state_name']
-        sen = value['senator']
-
-        if state_name not in sen_result_dict:
-            sen_result_dict[state_name] = {
-                'state_name': state_name,
-                'senators': []
-            }
-
-        sen_result_dict[state_name]['senators'].append(sen)
-
-    sen_result_list = list(sen_result_dict.values())
-
-    # Remove unnecessary escape codes from district names
-    districts = {
-        key: value.split('\u00a0')[0]
-        for key, value in districts.items()
-    }
-
-    new_state_info = {}
-    for key, district in districts.items():
-        new_state_info[key] = {
-            'state_name': district,
-            'representative': new_reps[key]
-        }
-
-    result_dict = {}
-    for key, value in new_state_info.items():
-        state_name = value['state_name']
-        rep = value['representative']
-
-        if state_name not in result_dict:
-            result_dict[state_name] = {
-                'state_name': state_name,
-                'house_delegation': []
-            }
-
-        result_dict[state_name]['house_delegation'].append(rep)
-
-    result_list = list(result_dict.values())
-
-    # Update the states.json data with the new representatives
-    for state in states_data:
-        state_name = state['state_name']
-        for result in result_list:
-            if result['state_name'] == state_name:
-                state['house_delegation'] = result['house_delegation']
-        for sen_result in sen_result_list:
-            if sen_result['state_name'] == state_name:
-                state['senators'] = sen_result['senators']
-
-    # Save the updated states data back to states.json
-        with open('states.json', 'w') as json_file:
-            json.dump(states_data, json_file, indent=2)
+    sen_result_list = return_rep_list(new_sen_info, 'senator', 'senators')
+    rep_result_list = return_rep_list(new_state_info, 'representative',
+                                      'house_delegation')
+    update_all(states_data, rep_result_list, sen_result_list)
 
 
 if __name__ == "__main__":
